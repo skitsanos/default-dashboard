21f2205292abe49e7008c5148a3a190c022cb48f
skitsanos <info@skitsanos.com>
2023-03-30 17:43:57 +0300

    chore: packages versions bumps, added users api mock

---
+++ mock/users.js
@@ -0,0 +1,49 @@
+import chance from 'chance';
+import {existsSync, mkdirSync, readFileSync, writeFileSync} from 'fs';
+
+const generateUser = function* ()
+{
+    yield {
+        key: chance().guid(),
+        name: chance().name(),
+        email: chance().email(),
+        uuiid: chance().guid()
+    };
+};
+
+export default {
+    'GET /api/users': (req, res) =>
+    {
+        const {skip = 0, pageSize = 10, q} = req.query;
+
+        if (!existsSync('./.data'))
+        {
+            mkdirSync('./.data');
+
+            const generatedUsers = [
+                {
+                    key: 'user-skitsanos',
+                    name: 'Evgenios Skitsanos',
+                    email: chance().email(),
+                    uuiid: chance().guid()
+                },
+                ...Array.from({length: 100}, () => generateUser().next().value)
+            ];
+
+            writeFileSync('./.data/users.json', JSON.stringify(generatedUsers));
+        }
+
+        const dataset = JSON.parse(readFileSync('./.data/users.json', 'utf8').toString());
+
+        const current = Number(skip);
+        const next = Number(skip) + Number(pageSize);
+
+        const resultingDataset = q ? dataset.filter(user => user.name.toLowerCase().includes(q.toLowerCase())).slice(current, next) : dataset.slice(current, next);
+
+        res.status(200).json({
+            data: resultingDataset,
+            total: dataset.length,
+            pageSize: Number(pageSize)
+        });
+    }
+};
