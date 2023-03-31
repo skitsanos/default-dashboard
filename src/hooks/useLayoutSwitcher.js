2887e09a9208fb190eec213433686d9dce029a80
skitsanos <info@skitsanos.com>
2023-03-31 11:04:52 +0300

    chore: added Layout Switcher hook

--- src/hooks/useLayoutSwitcher.js
+++ src/hooks/useLayoutSwitcher.js
@@ -2,17 +2,17 @@ import {create} from 'zustand';
 
 const APPLICATION_LAYOUT_KEY = 'layout';
 
-const useLayoutSwitcher = create((set) => ({
+const useLayoutSwitcher = create((set, get) => ({
     layout: localStorage.getItem(APPLICATION_LAYOUT_KEY) !== null
-        ? JSON.parse(localStorage.getItem(APPLICATION_LAYOUT_KEY))
+        ? localStorage.getItem(APPLICATION_LAYOUT_KEY)
         : 'top',
 
     toggle: () =>
     {
-        set({
-            layout: layout === 'top' ? 'left' : 'top'
-        });
-        localStorage.setItem(APPLICATION_LAYOUT_KEY, JSON.stringify(layout));
+        const newLayout = get().layout === 'top' ? 'left' : 'top';
+        set({layout: newLayout});
+
+        localStorage.setItem(APPLICATION_LAYOUT_KEY, newLayout);
     }
 }));
 
