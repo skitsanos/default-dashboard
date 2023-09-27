fe843af6d2f990024e6de1c14e4c213c7c097f27
skitsanos <info@skitsanos.com>
2023-09-27 19:40:29 +0300

    chore: file-preview block. cleanup

--- src/pages/files/$fileId/index.tsx
+++ src/pages/files/$fileId/index.tsx
@@ -2,6 +2,8 @@ import ContentArea from '@/components/ContentArea';
 import {useLocation, useParams} from 'umi';
 import '@/pages/files/thumbnails.less';
 import chance from 'chance';
+import {Card} from 'antd';
+import {useEffect, useState} from 'react';
 
 const generatePage = function* ()
 {
@@ -20,20 +22,60 @@ export default () =>
     const params = useParams();
     const {fileId} = params;
 
+    const [height, setHeight] = useState('auto'); // Initialize height to 'auto'
+
+    useEffect(() =>
+    {
+        const adjustHeight = () =>
+        {
+            const el = document.getElementById(fileId);
+            if (!el)
+            {
+                return;
+            }
+
+            // Get the y position of the second element
+            const yPos = el.getBoundingClientRect().top;
+
+            // Convert y position from pixels to viewport units (vh)
+            const yPosVh = yPos * (100 / window.innerHeight);
+
+            // Calculate the height
+            const newHeightVh = 96 - yPosVh;
+
+            setHeight(`${newHeightVh}vh`);
+        };
+
+        adjustHeight(); // Initial adjustment
+        window.addEventListener('resize', adjustHeight); // adjust on window resize
+
+        return () =>
+        {
+            // Cleanup - remove event listener when component unmounts
+            window.removeEventListener('resize', adjustHeight);
+        };
+    }, []);
+
     return <ContentArea title={'File Preview'} className={'v-box'}
                         subTitle={name}
                         breadcrumbs={[]}>
 
-        <div className={'file-viewer'}>
+        <Card>
+            some details
+        </Card>
 
-            <div className={'file-thumbnails-list'}>
-                {
-                    loadedDocument.map((_el, index) => <div key={`page-${index}`}
-                                                           className={'file-thumbnails-item'}></div>)
-                }
+        <Card>
+            <div id={fileId} className={'file-viewer'} style={{height}}>
+
+                <div className={'file-thumbnails-list'}>
+                    {
+                        loadedDocument.map((_el, index) => <div key={`page-${index}`}
+                                                                className={'file-thumbnails-item'}></div>)
+                    }
+                </div>
+
+                <div className={'file-page-preview'}>preview</div>
             </div>
-
-            <div className={'file-page-preview'}>preview</div>
-        </div>
+        </Card>
     </ContentArea>;
 }
