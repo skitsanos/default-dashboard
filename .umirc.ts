65b39bf7b272feaae39b4aae7beda054283f84df
skitsanos <info@skitsanos.com>
2024-03-29 08:27:53 +0200

    chore: code cleanup and updated Umi config

--- .umirc.ts
+++ .umirc.ts
@@ -3,24 +3,40 @@
  * Please refer to https://umijs.org/config for more details
  * or contact Evi Skitsanos https://www.linkedin.com/in/skitsanos/
  */
+import dayjs from 'dayjs';
+
+import manifest from './package.json';
 
 //import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
 
 export default ({
-    //favicon: '/assets/favicon.ico',
     title: 'My Dashboard',
 
-    // mfsu: {
-    //     strategy: 'normal'
-    // },
-
-    devtool: process.env.NODE_ENV === 'development' ? 'eval' : false,
+    favicons: [],
 
     styles: [
         //'https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i|IBM+Plex+Sans:300,400&display=swap',
         //'https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400&display=swap'
         'https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;300;400&display=swap'
     ],
+
+    headScripts: [{
+        content: 'console.log("Hello from the head! Modify your .umirc.ts")'
+    }],
+
+    define: {
+        // the following are the default values for the application, and they are defined in @types/typings.d.ts
+        APP_NAME: 'My Dashboard',
+        APP_VERSION: `${manifest.version} (beta/${dayjs().format('YYYY-MM-DD')})`,
+        FEATURE_SMTP_ENABLED: false,
+        FEATURE_SSL_ENABLED: true
+    },
+
+    mfsu: {},
+
+    deadCode: {},
+
+    devtool: process.env.NODE_ENV === 'development' ? 'eval' : false,
 
     proxy: {
         // '/api': {
---
+++ src/@types/typings.d.ts
@@ -0,0 +1,4 @@
+declare const APP_NAME: string;
+declare const APP_VERSION: string;
+declare const FEATURE_SMTP_ENABLED: boolean;
+declare const FEATURE_SSL_ENABLED: boolean;
